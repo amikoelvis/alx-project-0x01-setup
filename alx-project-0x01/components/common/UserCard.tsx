502a469134c2ccfd88798d80e219b1be10d89f71
Amiko Elvis <amikoelvis@gmail.com>
2025-07-16 11:43:07 +0300

    Updated UserCard.tsx with <UserProps>

--- alx-project-0x01/components/common/UserCard.tsx
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { UserProps } from "@/interfaces";
 
-interface UserCardProps {
-  user: UserProps;
-}
-
-const UserCard: React.FC<UserCardProps> = ({ user }) => {
+const UserCard: React.FC<{ user: UserProps }> = ({ user }) => {
   return (
     <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
       {/* Name & Username */}
