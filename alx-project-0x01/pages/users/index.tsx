cf594e0a56746c9947e37290b3f531fa86cac1bb
Amiko Elvis <amikoelvis@gmail.com>
2025-07-16 11:58:00 +0300

    Update

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -19,7 +19,7 @@ const Users: React.FC<UsersPageProps> = ({ posts }) => {
       <h1 className="text-3xl font-bold text-center mb-8">Users</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {posts.map((user) => (
-          <UserCard key={user.id} user={user} />
+          <UserCard key={user.id} {...user} />
         ))}
       </div>
     </div>
